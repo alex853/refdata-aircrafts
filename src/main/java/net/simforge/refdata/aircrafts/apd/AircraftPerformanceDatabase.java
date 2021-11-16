5a5d07cb2f5ecb53b129b9edd6fc11498e34dc1a
alex853 <alexxxey.kornev@gmail.com>
2021-11-17 01:30:59 +0400

    BM added

--- src/main/java/net/simforge/refdata/aircrafts/apd/AircraftPerformanceDatabase.java
+++ src/main/java/net/simforge/refdata/aircrafts/apd/AircraftPerformanceDatabase.java
@@ -4,6 +4,7 @@ import com.google.common.base.Preconditions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.simforge.commons.io.IOHelper;
+import net.simforge.commons.legacy.BM;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,28 +20,36 @@ public class AircraftPerformanceDatabase {
     private static final Map<String, Optional<AircraftPerformance>> data = new HashMap<>();
 
     public static Optional<AircraftPerformance> getPerformance(String icaoCode) {
-        Preconditions.checkNotNull(icaoCode);
-        icaoCode = icaoCode.toUpperCase();
-
-        Optional<AircraftPerformance> found = data.get(icaoCode);
-        //noinspection OptionalAssignedToNull
-        if (found != null) {
-            return found;
-        }
-
-        AircraftPerformance loaded = null;
+        BM.start("AircraftPerformanceDatabase.getPerformance");
         try {
-            loaded = load(icaoCode);
-        } catch (Exception e) {
-            logger.error("Unable to load aircraft performance for type " + icaoCode, e);
-        }
+            Preconditions.checkNotNull(icaoCode);
+            icaoCode = icaoCode.toUpperCase();
 
-        Optional<AircraftPerformance> result = Optional.ofNullable(loaded);
-        synchronized (data) {
-            data.put(icaoCode, result);
-        }
+            Optional<AircraftPerformance> found = data.get(icaoCode);
+            //noinspection OptionalAssignedToNull
+            if (found != null) {
+                return found;
+            }
 
-        return result;
+            AircraftPerformance loaded = null;
+            BM.start("AircraftPerformanceDatabase.load");
+            try {
+                loaded = load(icaoCode);
+            } catch (Exception e) {
+                logger.error("Unable to load aircraft performance for type " + icaoCode, e);
+            } finally {
+                BM.stop();
+            }
+
+            Optional<AircraftPerformance> result = Optional.ofNullable(loaded);
+            synchronized (data) {
+                data.put(icaoCode, result);
+            }
+
+            return result;
+        } finally {
+            BM.stop();
+        }
     }
 
     private static AircraftPerformance load(String icaoCode) throws IOException {
